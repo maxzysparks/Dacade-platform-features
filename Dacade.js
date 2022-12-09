16cf78c37e4c573ea03f39364a3a56faed8cccc4
4sight <80678178+maxzysparks@users.noreply.github.com>
2022-12-09 01:08:08 +0100

    Create Dacade.js

---
+++ Dacade.js
@@ -0,0 +1,23 @@
+// Dacade platform
+
+// Function to handle cUSD rewards
+function handleCUSDRewards(userId, walletOption, customAddress) {
+  // Get user's cUSD balance
+  let cUSDBalance = getCUSDBalance(userId);
+
+  // Check if user wants to use Wallet Connect
+  if (walletOption == "wallet_connect") {
+    // Get user's Wallet Connect address
+    let walletConnectAddress = getWalletConnectAddress(userId);
+    // Add cUSD rewards to user's Wallet Connect address
+    addCUSDRewardsToAddress(walletConnectAddress, cUSDBalance);
+  } else {
+    // User wants to use a custom address
+    // Add cUSD rewards to the custom address
+    addCUSDRewardsToAddress(customAddress, cUSDBalance);
+  }
+}
+
+// Example usage
+handleCUSDRewards(123456, "wallet_connect", ""); // Will add rewards to user's Wallet Connect address
+handleCUSDRewards(123456, "custom_address", "0x123..."); // Will add rewards to the specified custom address
